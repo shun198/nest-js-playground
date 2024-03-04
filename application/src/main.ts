10a6532be04eb1496fb040c80cb0ade93911358a
s.hirose <shunichi_hirose@pj100.biz>
2024-03-04 17:14:17 +0900

    👍 add create user logic

--- application/src/main.ts
+++ application/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Report API Project')
+    .setDescription('The Report API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
