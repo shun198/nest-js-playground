3ea23e65060be11a4e4b48b4c29c57565a33b43c
s.hirose <shunichi_hirose@pj100.biz>
2024-03-05 11:41:22 +0900

    👍 update to mycv

--- mycv/src/users/users.service.ts
+++ mycv/src/users/users.service.ts
@@ -13,14 +13,21 @@ export class UsersService {
   }
 
   findOne(id: number) {
-    return this.repo.findOne(id);
+    return this.repo.findOneBy({ id });
   }
 
-  find(email: string) {
-    return this.repo.find({ email });
-  }
+  // find(email: string) {
+  //   return this.repo.find({ email });
+  // }
 
-  update() {}
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
 
   remove() {}
 }
