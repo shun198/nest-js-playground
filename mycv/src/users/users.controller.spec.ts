b52a11ed02ae4543566a681c5cdcddec0f7fe1bb
s.hirose <shunichi_hirose@pj100.biz>
2024-03-19 07:48:04 +0900

    👍 add not found test

---
+++ mycv/src/users/users.controller.spec.ts
@@ -0,0 +1,69 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { NotFoundException } from '@nestjs/common';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
+
+  beforeEach(async () => {
+    fakeUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'example.com',
+          password: 'test',
+        } as User);
+      },
+      find: (email: string) => {
+        return Promise.resolve([{ id: 1, email, password: 'test' } as User]);
+      },
+      // remove: () => {},
+      // update: () => {},
+    };
+    fakeAuthService = {
+      // signup: () => {},
+      // signin: () => {},
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('findAllUserを実行するとユーザの配列が返されることを確認する', async () => {
+    const users = await controller.findAllUsers('example.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('example.com');
+  });
+
+  it('findUserを実行するとユーザのオブジェクトが返されることを確認する', async () => {
+    const user = await controller.findUser(1);
+    expect(user).toBeDefined();
+  });
+
+  it('存在しないユーザIDに対してfindUserを実行するとエラーになることを確認する', async () => {
+    fakeUsersService.findOne = () => null;
+    await expect(controller.findUser(1)).rejects.toThrow(NotFoundException);
+  });
+});
