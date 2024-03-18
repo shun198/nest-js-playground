2ad9b81946df23d7f921cc5cdba7fbe37f6d0bb8
shun198 <shunhiroseluvmri@gmail.com>
2024-03-18 15:39:20 +0900

    👍 modify test files

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -1,24 +1,48 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
-it('Auth Serviceのインスタンスを作成できる', async () => {
-  const fakeUsersService = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
-  };
+describe('AuthService', () => {
+  let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      },
-    ],
-  }).compile();
+  beforeEach(async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
+    service = module.get(AuthService);
+  });
 
-  const service = module.get(AuthService);
-  expect(service).toBeDefined();
+  it('Auth Serviceのインスタンスを作成できる', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('ソルト&ハッシュ化されたパスワードを持つユーザを作成', async () => {
+    const user = await service.signup('example.com', 'test');
+    expect(user.password).not.toEqual('test');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('すでに使用されたメールアドレスでサインアップした時', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    await expect(service.signup('example.com', 'test')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
 });
