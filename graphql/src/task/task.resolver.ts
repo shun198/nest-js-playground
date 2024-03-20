3953013967cac64df3532eb9c4176d91e507802f
shun198 <shunhiroseluvmri@gmail.com>
2024-03-20 14:58:58 +0900

    👍 add update and delete mutation

--- graphql/src/task/task.resolver.ts
+++ graphql/src/task/task.resolver.ts
@@ -1,11 +1,12 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { TaskService } from './task.service';
 import { Task as TaskModel } from './models/task.model';
 import { CreateTaskInput } from './dto/createTask.input';
+import { UpdateTaskInput } from './dto/updateTask.input';
 
 @Resolver()
 export class TaskResolver {
-  constructor(private readonly taskService: TaskService) {}
+  constructor(private readonly taskService: TaskService) { }
 
   @Query(() => [TaskModel], {
     nullable: 'items',
@@ -20,5 +21,19 @@ export class TaskResolver {
     @Args('createTaskInput') createTaskInput: CreateTaskInput,
   ): Promise<TaskModel> {
     return await this.taskService.createTask(createTaskInput);
+  }
+
+  @Mutation(() => TaskModel, { description: 'タスク更新用Mutation' })
+  async updateTask(
+    @Args('updateTaskInput') updateTaskInput: UpdateTaskInput,
+  ): Promise<TaskModel> {
+    return await this.taskService.updateTask(updateTaskInput);
+  }
+
+  @Mutation(() => TaskModel, { description: 'タスク削除用Mutation' })
+  async deleteTask(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<TaskModel> {
+    return await this.taskService.deleteTask(id);
   }
 }
--- graphql/src/task/task.service.ts
+++ graphql/src/task/task.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { CreateTaskInput } from './dto/createTask.input';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Task } from '@prisma/client';
+import { UpdateTaskInput } from './dto/updateTask.input';
 
 @Injectable()
 export class TaskService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) { }
 
   async getTasks(): Promise<Task[]> {
     return await this.prismaService.task.findMany();
@@ -19,6 +20,25 @@ export class TaskService {
         dueDate,
         description,
       },
+    });
+  }
+
+  async updateTask(updateTaskInput: UpdateTaskInput): Promise<Task> {
+    const { id, name, dueDate, status, description } = updateTaskInput;
+    return await this.prismaService.task.update({
+      data: {
+        name,
+        dueDate,
+        status,
+        description,
+      },
+      where: { id },
+    });
+  }
+
+  async deleteTask(id: number): Promise<Task> {
+    return await this.prismaService.task.delete({
+      where: { id },
     });
   }
 }
