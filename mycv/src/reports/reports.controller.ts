e426b0e38d2f2c42ca294d6159e366b9b4c3f593
shun198 <shunhiroseluvmri@gmail.com>
2024-03-25 07:27:09 +0900

    ✨ add create report api

--- mycv/src/reports/reports.controller.ts
+++ mycv/src/reports/reports.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dto/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { ApiTags, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('reports')
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        make: {
+          type: 'string',
+          default: 'toyota',
+        },
+        model: {
+          type: 'string',
+          default: 'corolla',
+        },
+        year: {
+          type: 'int',
+          default: '1980',
+        },
+        mileage: {
+          type: 'int',
+          default: 100000,
+        },
+        lng: {
+          type: 'int',
+          default: 0,
+        },
+        lat: {
+          type: 'int',
+          default: 0,
+        },
+        price: {
+          type: 'int',
+          default: 500000,
+        },
+      },
+    },
+  })
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
+}
